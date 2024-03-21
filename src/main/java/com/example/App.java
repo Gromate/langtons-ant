b788f48a17bac431dd9c7086fafe7c8d872df64f
Mateusz Grochowski <groszek20027@gmail.com>
2024-03-21 18:58:26 +0100

    Rotating squares

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,32 +1,41 @@
 package com.example;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    Group root;
+    int x;
 
     @Override
     public void start(Stage stage) {
-        Group root = new Group();
-        var scene = new Scene(root, 640, 480);
+        root = new Group();
+        var scene = new Scene(root, 1280, 530);
+        x = 0;
+
+        int size = 10;
+        int spacing = -1;
 
         for (int i=0; i < 100; i++) {
             for (int j=0; j< 100; j++) {
                 Rectangle rectangle = new Rectangle();
-                rectangle.setX(i * 11);
-                rectangle.setY(j * 11);
-                rectangle.setHeight(10);
-                rectangle.setWidth(10);
-                rectangle.setFill(Color.BLACK);
+                rectangle.setX(i * (size+spacing));
+                rectangle.setY(j * (size+spacing));
+                rectangle.setHeight(size);
+                rectangle.setWidth(size);
+                rectangle.setFill(Color.DARKSLATEGRAY);
 
                 root.getChildren().add(rectangle);
             }
         }
+        AnimationTimer tm = new TimerMethod();
+        tm.start();
 
         stage.setResizable(false);
         stage.setScene(scene);
@@ -35,6 +44,21 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private class TimerMethod extends AnimationTimer {
+        int klatka; 
+
+        @Override
+        public void handle(long arg0) {
+            for (Node children : root.getChildren()) {
+                klatka++;
+                    x ++;
+                    ((Rectangle)children).setRotate(x/10000);
+                    klatka = 0;
+            }
+        }
+
     }
 
 }
